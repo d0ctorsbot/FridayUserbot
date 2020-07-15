dcb1de8bd22b02b4c5d6c731917cfdfc21316388
Nimeshchandhra1 <68285734+Nimeshchandhra1@users.noreply.github.com>
2020-07-15 11:49:17 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,35 +1,28 @@
 """Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
-# @starkxD
+#IMG CREDITS: @WhySooSerious
 import asyncio
 from telethon import events
-from telethon.tl.types import ChannelParticipantsAdmins
-from platform import uname
+from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
-from userbot.utils import admin_cmd
-
-
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-ALIVE_STCR = "CgACAgEAAxkBAAEFY39fDpvDdgtjpN1dQVkwtsS-mIuW8gACsAIAAkjQcUSqr6K3-Sip-xoE"
-FRIDAY_ALIVE = ("`FRIDAY IS:` **ONLINE**\n\n"
-"**SYSTEM STATUS**\n\n"
-"`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n\n"
-"`DATABASE STATUS:` **Functional**\n\n"
-"**Current Branch** : `master`\n\n"
-"**Friday OS** : `3.14`\n\n"
-"**Current Sat** : `Sat-2.25`\n\n"
-f"**My Boss** : {DEFAULTUSER} \n\n"
-"**Heroku Database** : `AWS - Working Properly`\n\n"
+from telethon.tl.types import ChannelParticipantsAdmins
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
+PM_IMG = "https://telegra.ph/file/717b3eff2ba34d1de6b98.png"
+pm_caption = "`FRIDAY IS:` **ONLINE**\n\n"
+pm_caption += "**SYSTEM STATUS**\n"
+pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
+pm_caption += "`DATABASE STATUS:` **Functional**\n"
+pm_caption += "**Current Branch** : `master`\n"
+pm_caption += "**Friday OS** : `3.14`\n"
+pm_caption += "**Current Sat** : `Sat-2.25`\n"
+pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
+pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
 
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
-
 async def amireallyalive(alive):
-
     chat = await alive.get_chat()
-
     await alive.delete()
-
     """ For .alive command, check if the bot is running.  """
-
-    await borg.send_sticker(chat, ALIVE_STCR)
-    await borg.send_message(chat, FRIDAY_ALIVE
+    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+    await alive.delete() 
+    
